a51ec918456ff92ac2573c9fc3e8dce8dd8dc787
Dmitry Korovin <korovin.ds@aurrency.com>
2021-04-15 14:24:34 +0300

    Mint added

--- contracts/ShardedToken.sol
+++ contracts/ShardedToken.sol
@@ -1,15 +1,30 @@
 pragma solidity ^0.8.0;
 
-contract ShardedToken
+import "./_lib/openzeppelin-contracts/contracts/access/Ownable.sol";
+
+contract ShardedToken is Ownable
 {
 	event TransferToken(address indexed from, address indexed to, uint256 id, uint256 value);
 	event Transfer(address indexed from, address indexed to, uint value);
 
 	mapping (uint => mapping (address => uint)) public balances;
 
-	constructor ()
+	constructor () Ownable()
 	{
 
+	}
+
+	function balanceOf(address owner, uint id) public returns (uint)
+	{
+		return balances[id][owner];
+	}
+
+	function mint(address who, uint id, uint value) onlyOwner public
+	{
+		balances[id][who] += value;
+
+		emit TransferToken(address(0), who, id, value);
+		emit Transfer(address(0), who, value);
 	}
 
 	function transfer(address to, uint[] calldata ids, uint[] calldata values) public
